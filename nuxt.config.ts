d1d1a23f83341b305edf10e69075fed0e295a1d1
ELMM <elmm.programing@gmail.com>
2024-11-05 20:04:55 -0500

    Primeicons add and use it in nuxt config, runtime env variable apiBase added and toast added to the app.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+ runtimeConfig: {
+    public: {
+      apiBase: '', // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+    }
+  }, 
    modules: ['nuxt-primevue', '@nuxtjs/tailwindcss'],
       primevue: {
     cssLayerOrder: 'tailwind-base, primevue, tailwind-utilities',
@@ -12,5 +17,5 @@ export default defineNuxtConfig({
     } 
         /* Options */
     },
-  css: ['primevue/resources/themes/aura-light-green/theme.css','assets/css/tailwind.css']
+  css: ['primevue/resources/themes/aura-light-green/theme.css','assets/css/tailwind.css','primeicons/primeicons.css']
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="flex flex-col mx-auto h-screen gap-3  justify-center ">
-    <div class="flex flex-row mx-auto w-4/5 h-2/6 gap-2  ">
-      <NuxtLink class="basis-3/6 h-[100%] " to="/users">
-        <Button outlined class="border-2 w-[100%] h-[100%]   justify-center   ">
-          Users
-        </Button>
-      </NuxtLink>
-      <NuxtLink class="basis-3/6 h-[100%] " to="/roles">
-        <Button outlined class="border-2 w-[100%] h-[100%]   justify-center   ">
-          Roles
+  <div class="h-screen">
+    <div class="text-center  h-[7%] text-white font-bold text-2xl py-5 bg-[#10b981]">Policy Base Auth</div>
+    <div class="flex flex-col  h-[93%]  gap-3  justify-center ">
+      <div class="flex flex-row mx-auto w-4/5 h-2/6 gap-2  ">
+        <NuxtLink class="basis-3/6 h-[100%] " to="/users">
+          <Button outlined class="border-2 w-[100%] h-[100%]   justify-center   ">
+            Users
+          </Button>
+        </NuxtLink>
+        <NuxtLink class="basis-3/6 h-[100%] " to="/roles">
+          <Button outlined class="border-2 w-[100%] h-[100%]   justify-center   ">
+            Roles
+          </Button>
+        </NuxtLink>
 
-        </Button>
-      </NuxtLink>
+      </div>
+      <div class="flex flex-row mx-auto w-4/5 h-2/6 gap-2  ">
+        <NuxtLink class="basis-3/6 h-[100%] " to="/resources">
+          <Button outlined class="border-2 w-[100%] h-[100%]   justify-center   ">
+            Resources
+          </Button>
+        </NuxtLink>
+        <NuxtLink class="basis-3/6 h-[100%] " to="/permissions">
+          <Button outlined class="border-2 w-[100%] h-[100%]   justify-center   ">
+            Permissions
+          </Button>
+        </NuxtLink>
 
-    </div>
-    <div class="flex flex-row mx-auto w-4/5 h-2/6 gap-2  ">
-      <NuxtLink class="basis-3/6 h-[100%] " to="/resources">
-        <Button outlined class="border-2 w-[100%] h-[100%]   justify-center   ">
-          Resources
-
-        </Button>
-      </NuxtLink>
-      <NuxtLink class="basis-3/6 h-[100%] " to="/permissions">
-        <Button outlined class="border-2 w-[100%] h-[100%]   justify-center   ">
-          Permissions
-
-        </Button>
-      </NuxtLink>
-
+      </div>
     </div>
   </div>
 
